6f829b2e35f40a14888967766ab578a1212532fd
jarn78 <60463751+jarn78@users.noreply.github.com>
2023-12-03 12:34:34 +0100

    Update veml7700.h

--- esphome/components/VEML7700/veml7700.h
+++ esphome/components/VEML7700/veml7700.h
@@ -22,6 +22,60 @@ enum VEML7700IntegrationTime {
   VEML7700_IT_50MS 0x08  ///< ALS intetgration time 50ms
   VEML7700_IT_25MS 0x0C  ///< ALS intetgration time 25ms
 };
+class VEML7700Sensor : public sensor::Sensor, public PollingComponent, public i2c::I2CDevice {
+ public:
+  /** Set the time that sensor values should be accumulated for.
+   *
+   * Longer means more accurate, but also mean more power consumption.
+   *
+   * Possible values are:
+   *
+   *  - `sensor::TSL2561_INTEGRATION_14MS`
+   *  - `sensor::TSL2561_INTEGRATION_101MS`
+   *  - `sensor::TSL2561_INTEGRATION_402MS` (default)
+   *
+   * @param integration_time The new integration time.
+   */
+  void set_integration_time(VEML7700IntegrationTime integration_time);
 
+  /** Set the internal gain of the sensor. Can be useful for low-light conditions
+   *
+   * Possible values are:
+   *
+   *  - `sensor::TSL2561_GAIN_1X` (default)
+   *  - `sensor::TSL2561_GAIN_16X`
+   *
+   * @param gain The new gain.
+   */
+  void set_gain(VEML7700Gain gain);
+
+  /** The "CS" package of this sensor has a slightly different formula for
+   * converting the raw values. Use this setting to indicate that this is a CS
+   * package. Defaults to false (not a CS package)
+   *
+   * @param package_cs Is this a CS package.
+   */
+ // void set_is_cs_package(bool package_cs);
+
+  // ========== INTERNAL METHODS ==========
+  // (In most use cases you won't need these)
+  void setup() override;
+  void dump_config() override;
+  void update() override;
+  float get_setup_priority() const override;
+
+  bool tsl2561_read_byte(uint8_t a_register, uint8_t *value);
+  bool tsl2561_read_uint(uint8_t a_register, uint16_t *value);
+  bool tsl2561_write_byte(uint8_t a_register, uint8_t value);
+
+ protected:
+  float get_integration_time_ms_();
+  void read_data_();
+  float calculate_lx_(uint16_t ch0, uint16_t ch1);
+
+  TSL2561IntegrationTime integration_time_{TSL2561_INTEGRATION_402MS};
+  TSL2561Gain gain_{TSL2561_GAIN_1X};
+
+};
 }  // namespace veml7700
 }  // namespace esphome
